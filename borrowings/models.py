6ebd79ae1378ced5853ab188ecdbcdcdcf7fb839
Yuliana <pashkova.yuliana@gmail.com>
2024-04-22 10:56:47 +0300

    add borrowing model with constraints on dates & is_active property

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,38 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(
+        default=date.today,
+    )
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+    book_id = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowings"
+    )
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="borrowings"
+    )
+
+    @property
+    def is_active(self):
+        return not self.actual_return_date
+
+    def clean(self):
+        if self.expected_return_date <= self.borrow_date:
+            raise ValidationError("Expected return date must be after borrow date.")
+
+        if self.actual_return_date and self.actual_return_date < self.borrow_date:
+            raise ValidationError("Actual return date must not be before borrow date.")
+
+    def __str__(self):
+        return f"{self.book_id.title} borrowed by {self.user_id.email} on {self.borrow_date}"
