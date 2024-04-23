cd44d03e0106b6e6ca9afddca85fceb5e5612891
Yuliana <pashkova.yuliana@gmail.com>
2024-04-23 19:43:41 +0300

    implement create borrowing serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -35,3 +35,31 @@ class BorrowingListSerializer(BorrowingDetailSerializer):
             "user",
             "is_active",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "borrow_date",
+            "expected_return_date",
+        )
+
+    def validate(self, data):
+        book = data.get("book")
+        if book.inventory == 0:
+            raise serializers.ValidationError("This book is out of stock")
+        return data
+
+    def create(self, validated_data):
+        book = validated_data.get("book")
+        book.inventory -= 1
+        book.save()
+
+        borrowing = Borrowing.objects.create(
+            user=self.context["request"].user,
+            **validated_data,
+        )
+        return borrowing
